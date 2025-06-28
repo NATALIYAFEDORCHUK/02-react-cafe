6a62db8e59f2b73173e73e0bdab87b777b4ca13a
Fedorchuk Nataliia <fedorchuk1978nata@gmail.com>
2025-06-28 22:48:38 +0300

    correct structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
-import type { Votes, VoteType } from "../types/votes";
+import type { Votes, VoteType } from "../../types/votes";
 import VoteStatus from "../VoteStatus/VoteStatus";
 import Notification from "../Notification/Notification";
 
